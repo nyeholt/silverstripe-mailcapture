cc97e0a62da26985af367ca5d1106182cda33e66
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-06-10 12:14:09 +1000

    NEW prune emails task

---
+++ code/tasks/PruneEmailsTask.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author <marcus@silverstripe.com.au>
+ * @license BSD License http://www.silverstripe.org/bsd-license
+ */
+class PruneEmailsTask extends BuildTask {
+	public function run($request) {
+		if (Permission::check('ADMIN')) {
+			$since = date('Y-m-d H:i:s', strtotime('-1 month'));
+			$list = CapturedEmail::get()->filter('Created:LessThan', $since);
+			echo "Deleting " . $list->count() . " captured emails (if confirm flag set)<br/>\n";
+			
+			if ($request->getVar('confirm')) {
+				$list->removeAll();
+			}
+		}
+	}
+}
