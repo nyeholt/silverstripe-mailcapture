25f1a0b68ac19cd598cb3f2b026771563e623c38
Stephen McMahon <stephen@symbiote.com.au>
2019-04-17 16:39:13 +1000

    Fix(CaptureMailer) rewrite CaptureMailer to match SwiftMailer's definition

--- src/Email/CaptureMailer.php
+++ src/Email/CaptureMailer.php
@@ -33,29 +33,35 @@ class CaptureMailer extends SwiftMailer {
 		$this->send = $item;
 	}
 
-	/**
-	 * @param string $to Email recipient
-	 * @param string $from Email from
-	 * @param string $subject Subject text
-	 * @param string $plainContent Plain text content
-	 * @param array $attachedFiles List of attached files
-	 * @param array $customHeaders List of custom headers
-	 * @return mixed Return false if failure, or list of arguments if success
-	 */
-	public function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customHeaders = false) {
-		if ($this->captureEmails) {
+    /**
+     * @param \SilverStripe\Control\Email\Email $message
+     * @return bool Whether the sending was "successful" or not
+     */
+    public function send($message)
+    {
+        if ($this->captureEmails) {
+
+            $formatEmailAddress = function (array $emails): string
+            {
+                $return = '';
+                foreach ($emails as $address => $title) {
+                    $return .= "$address";
+                    if ($title) {
+                        $return .= " <$title>";
+                    }
+                    $return .= ", ";
+                }
+                return $return;
+            };
+
 			$mail = new CapturedEmail();
-			$mail->To = $to;
-			$mail->From = $from;
-			$mail->Subject = $subject;
+			$mail->To = $formatEmailAddress($message->getTo());
+			$mail->From = $formatEmailAddress($message->getFrom());
+			$mail->ReplyTo = $message->getReplyTo();
+			$mail->Subject = $message->getSubject();
+            $mail->Headers = $message->getSwiftMessage()->getHeaders()->toString();
 
-			if (is_array($customHeaders)) {
-				foreach ($customHeaders as $header => $val) {
-					$mail->Headers .= "$header: $val \n";
-				}
-			}
-
-			$mail->PlainText = $plainContent;
+			$mail->Content = $message->getBody();
 
 			if ($this->send) {
 				$mail->SendID = $this->send->ID;
@@ -64,78 +70,10 @@ class CaptureMailer extends SwiftMailer {
 			$mail->write();
 		}
 
-		if ($this->outboundMailer) {
-			return $this->outboundMailer->sendPlain(
-				$to,
-				$from,
-				$subject,
-				$plainContent,
-				$attachedFiles,
-				$customHeaders
-			);
+        if ($this->sendMailOutbound) {
+			return parent::send($message);
 		}
 
-		return true;
-	}
-
-	/**
-	 * Send a multi-part HTML email
-	 * TestMailer will merely record that the email was asked to be sent, without sending anything.
-	 *
-	 * @param string $to Email recipient
-	 * @param string $from Email from
-	 * @param string $subject Subject text
-	 * @param string $htmlContent HTML Content
-	 * @param array $attachedFiles List of attachments
-	 * @param array $customHeaders User specified headers
-	 * @param string $plainContent Plain text content. If omitted, will be generated from $htmlContent
-	 * @return mixed Return false if failure, or list of arguments if success
-	 */
-	public function sendHTML(
-		$to,
-		$from,
-		$subject,
-		$htmlContent,
-		$attachedFiles = false,
-		$customHeaders = false,
-		$plainContent = false,
-		$inlineImages = false
-	) {
-		if ($this->captureEmails) {
-			$mail = new CapturedEmail();
-			$mail->To = $to;
-			$mail->From = $from;
-			$mail->Subject = $subject;
-
-			if (is_array($customHeaders)) {
-				foreach ($customHeaders as $header => $val) {
-					$mail->Headers .= "$header: $val \n";
-				}
-			}
-
-			$mail->Content = $htmlContent;
-			$mail->PlainText = $plainContent;
-
-			if ($this->send) {
-				$mail->SendID = $this->send->ID;
-			}
-
-			$mail->write();
-		}
-
-		if ($this->outboundMailer) {
-			return $this->outboundMailer->sendHTML(
-				$to,
-				$from,
-				$subject,
-				$htmlContent,
-				$attachedFiles,
-				$customHeaders,
-				$plainContent,
-				$inlineImages
-			);
-		}
-
-		return true;
-	}
+        return true;
+    }
 }
